d3748b6464156002fb77f2c9d45cf21cf2000178
Max Pokidov <temperlabs@gmail.com>
2024-12-26 23:21:41 +0300

    deploy attempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path';
+import { resolve } from 'path'
 console.log('build')
-console.log(process.env.VITE_APP_BASE_URL); // Для Node.js
+console.log(import.meta.env.VITE_APP_BASE_URL) // Для Node.js
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+  },
   base: process.env.VITE_APP_BASE_URL || '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
       '@components': resolve(__dirname, 'src/components'),
       '@pages': resolve(__dirname, 'src/pages'),
-      '@entities': resolve(__dirname, 'src/entities'),
-    },
-  },
+      '@entities': resolve(__dirname, 'src/entities')
+    }
+  }
 })
